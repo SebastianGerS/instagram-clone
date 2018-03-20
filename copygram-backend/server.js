3ee29ec8d29063d91d1bad554ab32b4d34f8e2fd
SebastianGerS <sebastian.gerstel.sollerman@chasacademy.se>
2018-03-20 15:14:34 +0100

    add func to keep herkouserver from sleeping (if it goes to sleep all uploaded images are lost

--- copygram-backend/server.js
+++ copygram-backend/server.js
@@ -4,3 +4,7 @@ var server = app.listen(process.env.PORT || 5000, function() {
   console.log('Express is now listening to port:' + (process.env.PORT || 5000));
 });
 
+setInterval(function() {
+  app.get('https://copygram-backend.herokuapp.com/mediaItems/all')
+}, 300000); /*keeps the conection alive necasery becous heroku only 
+precist deploydfiles not files uploded and saved with multer*/
