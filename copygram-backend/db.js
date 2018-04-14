fd23c8886344cfa3484d4977d011a81a5ef6df55
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-04-14 15:13:08 +0200

    change default port for server

--- copygram-backend/db.js
+++ copygram-backend/db.js
@@ -3,6 +3,6 @@ var config = require('./config');
 
 mongoose.connect("mongodb://" + config.username + ":" + config.password + "@ds157528.mlab.com:57528/copygram");
 
-mongoose.connection.on('error', function(error) {
-  if(error) console.log('error while trying to connect to database');
+mongoose.connection.on('error', function (error) {
+  if (error) console.log('error while trying to connect to database');
 });
--- copygram-backend/server.js
+++ copygram-backend/server.js
@@ -1,5 +1,5 @@
-var app = require('./app.js');
+var app = require("./app.js");
 
-var server = app.listen(process.env.PORT || 5000, function() {
-  console.log('Express is now listening to port:' + (process.env.PORT || 5000));
+var server = app.listen(process.env.PORT || 3001, function() {
+  console.log("Express is now listening to port:" + (process.env.PORT || 3001));
 });
