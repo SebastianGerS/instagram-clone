530e6ae047f5fc699f95c08d280efecf5ceb529b
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-04-23 00:34:12 +0200

    fix bug where some routes coused app to crash when not logedin

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,22 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { Home, Profile, Explore, ErrorPage, SignUp, ItemUpload } from '../views';
+import {connect} from 'react-redux';
 
-
-const Root = () => {
-
-  return (
-    <Switch>
-      <Route exact path="/home" component={Home}/>
-      <Route exact path="/profile" component={Profile}/>
-      <Route exact path="/explore" component={Explore}/>
-      <Route exact path="/upload" component={ItemUpload} />
-      <Route exact path="/" component={Explore}/>
-      <Route path="/users/:userId" component={Profile}/>
-      <Route exact path="/register" component={SignUp}/>
-      <Route component={ErrorPage}/>
-    </Switch>
-  )
+const mapStateToProps = state => {
+  return { isLogedin: state.isLogedin};
 }
+
+class ConnectedRoot  extends Component {
+
+  render() {
+    return (
+      this.props.isLogedin ?
+      <Switch>
+        <Route exact path="/home" component={Home}/>
+        <Route exact path="/profile" component={Profile}/>
+        <Route exact path="/explore" component={Explore}/>
+        <Route exact path="/upload" component={ItemUpload} />
+        <Route exact path="/" component={Explore}/>
+        <Route path="/users/:userId" component={Profile}/>
+        <Route exact path="/register" component={SignUp}/>
+        <Route component={ErrorPage}/>
+      </Switch>
+      : 
+      <Switch>
+        <Route exact path="/explore" component={Explore}/>
+        <Route exact path="/" component={Explore}/>
+        <Route path="/users/:userId" component={Profile}/>
+        <Route exact path="/register" component={SignUp}/>
+        <Route component={ErrorPage}/>
+      </Switch>
+    )
+  }
+
+}
+
+const Root = connect(mapStateToProps)(ConnectedRoot);
 
 export default Root;
