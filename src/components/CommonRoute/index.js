7fd4e35aaf87a823395891d46f440465311c0809
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-07-22 23:12:00 +0200

    add wraper for Route component with layout

---
+++ src/components/CommonRoute/index.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import './styles.css';
+import { Header, Footer } from '../';
+import {Route} from 'react-router-dom';
+
+const CommonRoute =( {component: Component, ...rest}) => (
+  
+
+    <Route {...rest} render={props => 
+        (
+            <div className="app">
+                <Header />
+                <div className="wraper">
+                    <Component {...props}/>
+                </div>
+                <Footer/>
+            </div>
+        )
+    }/>
+)
+
+export default CommonRoute;
