8c36995b3345de86c8fa6c5f4940948a4607afcf
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-07-22 23:16:58 +0200

    replace App with Root component

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import {Provider} from 'react-redux';
 import Store from './data/Store';
-import { App } from './components';
+import {Root} from './containers';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 ReactDom.render(
   
   <Provider store={Store}>
-    <App/>
+    <Root/>
   </Provider>,
   
   document.getElementById('root')
